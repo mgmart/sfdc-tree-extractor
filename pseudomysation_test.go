152bc923484f455ac16ba433cdf0fb47ae4bcce0
Mario Martelli <cocoa@schnuddelhuddel.de>
2023-03-21 07:38:20 +0100

    Faker is used

---
+++ pseudomysation_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestFakerCompany(t *testing.T) {
+
+	got := fakeCompany()
+	want := ""
+	t.Errorf("got %v want %v", got, want)
+	if got == want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
